f5f2fa772499937efd356ac0a0d753ea93a163bb
utsavp <utsavparmar72@gmail.com>
2024-07-02 22:44:47 +0530

    changes

--- server.ts
+++ server.ts
@@ -21,11 +21,11 @@ const __filename: any = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3030;
 // Socket.IO event handling
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src/public', 'index.html'));
 });
 
 
